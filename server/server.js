383521c996ce284a7e3cfbd322916728739f2d0b
Ida Granholm <granholm.ida@gmail.com>
2020-09-18 15:22:23 +0200

    Create basic init for api-routes

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 let express = require("express");
+let apiRoutes = require("./api-routes");
 
 // Initialize server app
 let app = express();
@@ -8,6 +9,9 @@ var port = process.env.PORT || 8080;
 
 // Send message for default URL
 app.get("/", (req, res) => res.send("The server is live!"));
+
+// Use Api routes in the App
+app.use("/api", apiRoutes);
 
 // Launch server to listen to specified port
 app.listen(port, function () {
