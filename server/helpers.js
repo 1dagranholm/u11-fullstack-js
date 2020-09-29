6790cc930dbd0c8865f39fdc88a0ad9939209820
Ida Granholm <granholm.ida@gmail.com>
2020-09-29 22:37:45 +0200

    Fix eslint warnings

--- server/helpers.js
+++ server/helpers.js
@@ -1,29 +1,29 @@
 module.exports = {
-    restApiResponse: function (err, message, data = "", res = null, debug = false) {
-        if (err) {
-            if (debug) {
-                res.json({
-                    status: "error",
-                    message: message[0],
-                    debug: err,
-                });
-            } else {
-                res.json({
-                    status: "error",
-                    message: message[0],
-                });
-            }
-        } else if (data == 0) {
-            res.json({
-                status: "error",
-                message: "There's no data to retrieve",
-            });
-        } else {
-            res.json({
-                status: "success",
-                message: message[1],
-                data: data,
-            });
-        }
-    },
+  restApiResponse(err, message, data = '', res = null, debug = false) {
+    if (err) {
+      if (debug) {
+        res.json({
+          status: 'error',
+          message: message[0],
+          debug: err,
+        });
+      } else {
+        res.json({
+          status: 'error',
+          message: message[0],
+        });
+      }
+    } else if (data === 0) {
+      res.json({
+        status: 'error',
+        message: "There's no data to retrieve",
+      });
+    } else {
+      res.json({
+        status: 'success',
+        message: message[1],
+        data,
+      });
+    }
+  },
 };
--- server/userModel.js
+++ server/userModel.js
@@ -1,46 +1,47 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema(
-    {
-        userName: {
-            type: "string",
-            required: true,
-            unique: true,
-        },
-        password: {
-            type: "string",
-            pattern: "^(?:\\d+|[a-zA-Z]+|[!@#$%^&*]+)$",
-            required: true,
-        },
-        role: {
-            type: String,
-            enum: ["admin", "user"],
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        firstName: {
-            type: String,
-            required: true,
-        },
-        lastName: {
-            type: String,
-            required: true,
-        },
-        deletedAt: {
-            type: Date,
-        },
+  {
+    userName: {
+      type: 'string',
+      required: true,
+      unique: true,
     },
-    {
-        timestamps: true,
-    }
+    password: {
+      type: 'string',
+      pattern: '^(?:\\d+|[a-zA-Z]+|[!@#$%^&*]+)$',
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'user'],
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    deletedAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  },
 );
 
-const User = (module.exports = mongoose.model("User", userSchema));
+const User = mongoose.model('User', userSchema);
+module.exports = User;
 
 module.exports.get = function (callback, limit) {
-    User.find(callback).limit(limit);
+  User.find(callback).limit(limit);
 };
