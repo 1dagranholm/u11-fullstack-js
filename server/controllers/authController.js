6b463a339c7800b6795f58904236eba30e6a94a7
Ida Granholm <granholm.ida@gmail.com>
2020-10-23 12:20:03 +0200

    Permission check for deleted users when logging in inserted => not able to login.

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -79,6 +79,10 @@ exports.signup = (req, res) => {
         if (!user) {
           return res.status(404).send({ message: "User Not found." });
         }
+
+        if (user.deletedAt) {
+          return res.status(404).send({ message: "There's no user with this e-mail." });
+        }
   
         var passwordIsValid = bcrypt.compareSync(
           req.body.password,
