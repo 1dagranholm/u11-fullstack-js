bf028a2729f3e137eddcd1e01dfb4a59f8a386c1
Ida Granholm <granholm.ida@gmail.com>
2020-10-26 10:02:31 +0100

    Insert sticky footer

--- app/src/components/core/Footer.tsx
+++ app/src/components/core/Footer.tsx
@@ -3,15 +3,17 @@ import React from "react";
 const Footer = () => {
 
     return (
-        <footer className="footer mt-auto py-5">
-            <div className="container d-flex justify-content-around align-items-center">
-                <div className="d-flex">
-                        <span className="text-muted col-12">
-                            Get your Todo's together in this <strong>TodoApp</strong> by <a href="mailto:granholm.ida@gmail.com">Ida Granholm</a> @ Chas Academy.
-                        </span>
+        <section id="footer-section" className="fixed-bottom">
+            <footer id="bottom-footer" className="footer pt-4 pb-4">
+                <div className="container d-flex justify-content-around align-items-center">
+                    <div className="d-flex">
+                            <span id="footer-text" className="text-muted col-12">
+                                Get your Todo's together in this <strong>TodoApp</strong> by <a href="mailto:granholm.ida@gmail.com">Ida Granholm</a> @ Chas Academy.
+                            </span>
+                    </div>
                 </div>
-            </div>
-        </footer>
+            </footer>
+        </section>
     );
 };
 
