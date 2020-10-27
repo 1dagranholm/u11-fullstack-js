fe94db9b5957d867ec731951e8776678c212ee8d
Ida Granholm <49232330+1dagranholm@users.noreply.github.com>
2020-10-27 14:17:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ mkdir todo-app &&
 git clone https://github.com/1dagranholm/u11-fullstack-js.git ./todo-app &&
 cd ./todo-app/app && npm i &&
 cd ../server && npm i &&
-echo Install completed
-
+rm ../install.sh &&
+echo -------------------------
+echo Install completed.
+echo -------------------------
